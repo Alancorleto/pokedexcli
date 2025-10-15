a46ab02888c0e217f4497c7ed5e95aa1cf1c75ec
alancorleto <corletoalan@gmail.com>
2025-10-15 15:08:46 -0300

    Add support for commands, and the exit and help commands

--- main.go
+++ main.go
@@ -7,6 +7,29 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand
+
+func init() {
+	commands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Show help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
@@ -15,7 +38,7 @@ func main() {
 		input := scanner.Text()
 		words := cleanInput(input)
 		if len(words) > 0 {
-			fmt.Printf("%s\n", words[0])
+			processCommand(words[0])
 		}
 	}
 }
@@ -30,4 +53,26 @@ func cleanInput(text string) []string {
 		words[i] = strings.ToLower(words[i])
 	}
 	return words
+}
+
+func processCommand(command string) {
+	if cmd, exists := commands[command]; exists {
+		cmd.callback()
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("")
+	for _, cmd := range commands {
+		fmt.Printf("  %s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
 }
